bff4dd0e83f2b16f9786bdaafb0f49e71f9d11c6
Meet Vardoriya <64339539+mv1249@users.noreply.github.com>
2022-03-23 19:53:47 +0530

    Update waSendMessage.py

--- waSendMessage.py
+++ waSendMessage.py
@@ -5,9 +5,9 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 load_dotenv()
 
-ACCOUNT_SID = "ACd32b923452389686a101281f89886fda"
-AUTH_TOKEN = "31a0beedf0125d40796703a870c1383c"
-FROM = "whatsapp:+14155238886"
+ACCOUNT_SID = ""
+AUTH_TOKEN = ""
+FROM = ""
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
